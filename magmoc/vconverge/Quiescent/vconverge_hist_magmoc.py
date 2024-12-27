ae838724e031964dee016f6aacfaa1042c3a3b92
Rory Barnes <rory@proxima.astro.washington.edu>
2024-12-27 11:35:16 -0800

    Added new file unconverged.py to process vconverge runs that did not finish.
    Adding file Unconverged_Param_Dictionary.json for the quiescent magma ocean runs. They almost converged, but failed with explanation after 6000 trials.

--- magmoc/vconverge/Quiescent/vconverge_hist_magmoc.py
+++ magmoc/vconverge/Quiescent/vconverge_hist_magmoc.py
@@ -6,6 +6,7 @@ import vplot
 
 co2solfactor = 1e18
 co2mofactor = 1e21
+gyearsec = 3600*25*365.25*1e9
 
 def GetData(filename):
     with open(filename, 'r') as file:
@@ -28,84 +29,80 @@ def ExtractColumn(data,key):
 
 def NormHistPanel(column,color,x,y,label=None):
     column_clean = pd.Series(column).dropna()
-    counts, bin_edges = np.histogram(column_clean, bins=iNumBins)
+    column_filtered = column_clean[(column_clean >= 0)]
+    counts, bin_edges = np.histogram(column_filtered, bins=iNumBins)
 
     # Normalize the counts to fractions
-    fractions = counts / len(column_clean)
-
+    fractions = counts / len(column_filtered)
+    #print(column_filtered)
     # Clear the figure and re-plot the normalized histogram
     #ax[x,y].clear()  # Clear the previous plot
-    ax[x,y].step(bin_edges[:-1], fractions,where='mid', color=color,label=label)
+    ax[x,y].step(bin_edges[:-1], fractions, where='mid', color=color,label=label)
 
-both = GetData("RadTidal/output/Converged_Param_Dictionary.json")
-daSurfTempBoth = ExtractColumn(both,"b,SurfTemp,final")
-daWaterSolBoth = ExtractColumn(both,"b,WaterMassSol,final")
-daWaterMOBoth = ExtractColumn(both,"b,WaterMassMOAtm,final")
-daWaterAtmBoth = ExtractColumn(both,"b,PressWaterAtm,final")
-daOxyAtmBoth = ExtractColumn(both,"b,PressOxygenAtm,final")
-daFe2O3Both = ExtractColumn(both,"b,FracFe2O3Man,final")
-daCO2SolBoth = ExtractColumn(both,"b,CO2MassSol,final")
-daCO2MOBoth = ExtractColumn(both,"b,CO2MassMOAtm,final")
-daCO2AtmBoth = ExtractColumn(both,"b,PressCO2Atm,final")
-
-rad = GetData("RadOnly/output/Converged_Param_Dictionary.json")
-daSurfTempRad = ExtractColumn(rad,"b,SurfTemp,final")
-daWaterSolRad = ExtractColumn(rad,"b,WaterMassSol,final")
-daWaterMORad = ExtractColumn(rad,"b,WaterMassMOAtm,final")
-daWaterAtmRad = ExtractColumn(rad,"b,PressWaterAtm,final")
-daOxyAtmRad = ExtractColumn(rad,"b,PressOxygenAtm,final")
-daFe2O3Rad = ExtractColumn(rad,"b,FracFe2O3Man,final")
-daCO2SolRad = ExtractColumn(rad,"b,CO2MassSol,final")
-daCO2MORad = ExtractColumn(rad,"b,CO2MassMOAtm,final")
-daCO2AtmRad = ExtractColumn(rad,"b,PressCO2Atm,final")
+#out = GetData("vconverge_tmp/tmp_Converged_Param_Dictionary.json")
+out = GetData("Unconverged_Param_Dictionary.json")
+daAge = ExtractColumn(out,"Age,final")/gyearsec
+daSurfTemp = ExtractColumn(out,"b,SurfTemp,final")
+daWaterSol = ExtractColumn(out,"b,WaterMassSol,final")
+daWaterMO = ExtractColumn(out,"b,WaterMassMOAtm,final")
+daWaterAtm = ExtractColumn(out,"b,PressWaterAtm,final")
+daOxyAtm = ExtractColumn(out,"b,PressOxygenAtm,final")
+daFe2O3 = ExtractColumn(out,"b,FracFe2O3Man,final")
+daCO2Sol = ExtractColumn(out,"b,CO2MassSol,final")
+daCO2MO = ExtractColumn(out,"b,CO2MassMOAtm,final")
+daCO2Atm = ExtractColumn(out,"b,PressCO2Atm,final")
 
 iNumBins = 50
 
 fig, ax = plt.subplots(nrows = 3, ncols=3, figsize=(6.5, 7))
-NormHistPanel(daSurfTempBoth,'k',0,0)
-NormHistPanel(daSurfTempRad,vplot.colors.pale_blue,0,0)
-ax[0,0].set_xlabel('Surface Temp [K]')
-ax[0,0].set_ylabel('Number')
+# NormHistPanel(daSurfTemp,'k',0,0)
+# ax[0,0].set_xlabel('Surface Temp [K]')
+# ax[0,0].set_ylabel('Number')
+NormHistPanel(daAge,'k',0,0)
+ax[0,0].set_xlabel('Solidification Age [Gyr]')
+ax[0,0].set_ylabel('Fraction')
+ax[0,0].set_title('Magma Ocean')
 
-NormHistPanel(np.log10(daFe2O3Both),'k',0,1,label='Rad+Tides')
-NormHistPanel(np.log10(daFe2O3Rad),vplot.colors.pale_blue,0,1,label='Rad Only')
-ax[0,1].legend()
-ax[0,1].set_xlabel(r'log$_{10}$(Fe$_2$O$_3$ Fraction)')
+#NormHistPanel(np.log10(daFe2O3),'k',0,1)
+#ax[0,1].set_xlabel(r'log$_{10}$(Fe$_2$O$_3$ Fraction)')
+NormHistPanel(daFe2O3,'k',0,1)
+ax[0,1].set_xlabel(r'Mantle Fe$_2$O$_3$ Fraction')
 ax[0,1].set_ylabel('Fraction')    
+ax[0,1].set_title('Mantle')
 
-NormHistPanel(daOxyAtmBoth,'k',0,2)
-NormHistPanel(daOxyAtmRad,vplot.colors.pale_blue,0,2)
-ax[0,2].set_xlabel('Oxygen Atm. [bar]')
+NormHistPanel(daOxyAtm/1e3,'k',0,2)
+ax[0,2].set_xlabel('Oxygen Atm. [kbar]')
 ax[0,2].set_ylabel('Fraction')
+ax[0,2].set_title('Atmosphere')
 
-NormHistPanel(daWaterMOBoth,'k',1,0)
-NormHistPanel(daWaterMORad,vplot.colors.pale_blue,1,0)
+NormHistPanel(daWaterMO,'k',1,0)
 ax[1,0].set_xlabel('Water Magma Ocean [TO]')
 ax[1,0].set_ylabel('Fraction')
 
-NormHistPanel(daWaterSolBoth,'k',1,1)
-NormHistPanel(daWaterSolRad,vplot.colors.pale_blue,1,1)
+daWaterSol = daWaterSol[(daWaterSol >= 0) & (daWaterSol <= 5)]
+
+NormHistPanel(daWaterSol,'k',1,1)
 ax[1,1].set_xlabel('Water Mantle [TO]')
 ax[1,1].set_ylabel('Fraction')
 
-NormHistPanel(daWaterAtmBoth,'k',1,2)
-NormHistPanel(daWaterAtmRad,vplot.colors.pale_blue,1,2)
-ax[1,2].set_xlabel('Water Atm [bar]')
+daWaterAtm = daWaterAtm[(daWaterAtm >= 0) & (daWaterAtm <= 5e4)]
+
+NormHistPanel(daWaterAtm/1e3,'k',1,2)
+ax[1,2].set_xlabel('Water Atm [kbar]')
 ax[1,2].set_ylabel('Fraction')
 
-NormHistPanel(daCO2MOBoth/co2mofactor,'k',2,0)
-NormHistPanel(daCO2MORad/co2mofactor,vplot.colors.pale_blue,2,0)
+NormHistPanel(daCO2MO/co2mofactor,'k',2,0)
 ax[2,0].set_xlabel(r'CO$_2$ Magma Ocean [$10^{21}$kg]')
 ax[2,0].set_ylabel('Fraction')
 
-NormHistPanel(daCO2SolBoth/co2solfactor,'k',2,1)
-NormHistPanel(daCO2SolRad/co2solfactor,vplot.colors.pale_blue,2,1)
+daCO2Sol = daCO2Sol[(daCO2Sol >= 0) & (daCO2Sol <= 1e21)]
+
+NormHistPanel(daCO2Sol/co2solfactor,'k',2,1)
 ax[2,1].set_xlabel(r'CO$_2$ Mantle [$10^{18}$kg]')
 ax[2,1].set_ylabel('Fraction')
 
-NormHistPanel(daCO2AtmBoth,'k',2,2)
-NormHistPanel(daCO2AtmRad,vplot.colors.pale_blue,2,2)
-ax[2,2].set_xlabel(r'CO$_2$ Atm. [bar]')
+NormHistPanel(daCO2Atm/1e3,'k',2,2)
+ax[2,2].set_xlabel(r'CO$_2$ Atm. [kbar]')
 ax[2,2].set_ylabel('Fraction')
 
 
